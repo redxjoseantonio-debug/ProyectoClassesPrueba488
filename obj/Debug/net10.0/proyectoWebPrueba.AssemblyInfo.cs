5b53c66f24483e5a00bc23b67139708b159ae4cd
redxjoseantonio-debug <redxjoseantonio@gmail.com>
2026-06-07 15:36:39 -0600

    arreglando api XD

--- obj/Debug/net10.0/proyectoWebPrueba.AssemblyInfo.cs
+++ obj/Debug/net10.0/proyectoWebPrueba.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyectoWebPrueba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e69a1bbcaf4c0acf162bf195fe25ebecb576af7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1488a2a2d102b97c07a5ede9f3f3fe8727da309e")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyectoWebPrueba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyectoWebPrueba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
